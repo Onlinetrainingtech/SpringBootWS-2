f7a180fdb7cc61de738cf07d9f0a8ff7b718986a
OnlineTrainer <mohamedtechdata@gmail.com>
2022-09-23 07:44:27 +0530

    Initial commit

---
+++ Spring-boot-webB12/Spring-boot-webB12/src/main/java/com/xyz/SpringbootwebB12/SpringBootWebB12Application.java
@@ -0,0 +1,13 @@
+package com.xyz.SpringbootwebB12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebB12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebB12Application.class, args);
+	}
+
+}
---
+++ Spring-boot-webB12/Spring-boot-webB12/src/test/java/com/xyz/SpringbootwebB12/SpringBootWebB12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.SpringbootwebB12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebB12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
