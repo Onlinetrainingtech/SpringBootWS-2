af183b85460390d60d35e259a70258261f6c26b4
OnlineTrainer <mohamedtechdata@gmail.com>
2022-11-23 05:58:17 +0530

    23-11-2022

---
+++ SampleB15/src/main/java/com/example/demo/SampleB15Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleB15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleB15Application.class, args);
+	}
+
+}
---
+++ SpringStarterProjectB15/SpringStarterProjectB15/src/test/java/com/example/SpringStarterProjectB15/SpringStarterProjectB15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringStarterProjectB15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterProjectB15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
