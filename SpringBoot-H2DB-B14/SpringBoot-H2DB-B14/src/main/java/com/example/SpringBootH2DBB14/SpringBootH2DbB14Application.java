781b7580f03d729fd1d02baff594a633fd3afb2e
OnlineTrainer <mohamedtechdata@gmail.com>
2022-12-02 07:11:47 +0530

    2-12-2022

---
+++ SpringBoot-H2DB-B14/SpringBoot-H2DB-B14/src/main/java/com/example/SpringBootH2DBB14/SpringBootH2DbB14Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootH2DBB14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DbB14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2DbB14Application.class, args);
+	}
+
+}
---
+++ SpringBoot-H2DB-B14/SpringBoot-H2DB-B14/src/test/java/com/example/SpringBootH2DBB14/SpringBootH2DbB14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootH2DBB14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2DbB14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
