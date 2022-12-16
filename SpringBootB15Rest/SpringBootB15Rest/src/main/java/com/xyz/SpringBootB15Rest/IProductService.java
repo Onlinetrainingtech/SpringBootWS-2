f17016b16a67fe168b46ecf17003c8d977fa6a47
OnlineTrainer <mohamedtechdata@gmail.com>
2022-12-16 07:30:19 +0530

    16-12-2022

---
+++ SpringBootB15Rest/SpringBootB15Rest/src/main/java/com/xyz/SpringBootB15Rest/IProductService.java
@@ -0,0 +1,8 @@
+package com.xyz.SpringBootB15Rest;
+
+import java.util.List;
+
+public interface IProductService 
+{
+   List<Product>findAll();
+}
---
+++ SpringBootB15Rest/SpringBootB15Rest/src/main/java/com/xyz/SpringBootB15Rest/SpringBootB15RestApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.SpringBootB15Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootB15RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootB15RestApplication.class, args);
+	}
+
+}
