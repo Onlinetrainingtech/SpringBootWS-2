40069d3521ea2a4b73e34d9425b8fa1364014f15
OnlineTrainer <mohamedtechdata@gmail.com>
2023-01-18 07:32:40 +0530

    18-01-23

---
+++ SpringP1/SpringP1/src/test/java/com/example/SpringP1/SpringP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SpringP2Web/SpringP2Web/src/test/java/com/example/SpringP2Web/SpringP2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringP2Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringP2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
