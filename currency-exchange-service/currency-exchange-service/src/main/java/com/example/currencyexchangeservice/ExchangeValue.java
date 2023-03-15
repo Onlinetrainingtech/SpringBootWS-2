6a137520365eff9ab61800d5384a80a9f6fc89ad
xyz <xyz@example.com>
2023-03-15 07:50:04 +0530

    15-03-23

--- currency-exchange-service/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
@@ -1,25 +1,47 @@
 package com.example.currencyexchangeservice;
-
 import java.math.BigDecimal;  
+import javax.persistence.Column;  
+import javax.persistence.Entity;  
+import javax.persistence.Id;  
+import javax.persistence.Table;  
+import java.math.BigDecimal;  
+@Entity  
+@Table(name="Exchange_Value")  
 public class ExchangeValue   
 {  
+@Id  
+@Column(name="id")  
 private Long id;  
+@Column(name="currency_from")  
 private String from;  
+@Column(name="currency_to")  
 private String to;  
+@Column(name="conversion_multiple")  
 private BigDecimal conversionMultiple;  
-  
+@Column(name="port")  
+private int port;  
+//default conatructor  
 public ExchangeValue()  
 {     
 }  
 //generating constructor using fields  
-public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {  
+public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple)   
+{  
 super();  
 this.id = id;  
 this.from = from;  
 this.to = to;  
 this.conversionMultiple = conversionMultiple;  
 }  
-//generating getters  
+//generating getters and setters  
+public int getPort()   
+{  
+return port;  
+}  
+public void setPort(int port)   
+{  
+this.port = port;  
+}  
 public Long getId()   
 {  
 return id;  
