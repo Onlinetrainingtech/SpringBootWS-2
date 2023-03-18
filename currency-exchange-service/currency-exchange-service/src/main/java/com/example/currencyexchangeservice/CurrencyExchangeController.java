84c233d7fe4b73c5347a34c887ace7f4b6dd5eb2
xyz <xyz@example.com>
2023-03-18 22:16:21 +0530

    18-03-2023

--- currency-exchange-service/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,17 +1,25 @@
 package com.example.currencyexchangeservice;
-import java.math.BigDecimal;  
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;  
 import org.springframework.boot.autoconfigure.SpringBootApplication;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.PathVariable;  
-import org.springframework.web.bind.annotation.RestController;  
-@SpringBootApplication  
-@RestController   
-public class CurrencyExchangeController   
-{  
-@GetMapping("/currency-exchange/from/{from}/to/{to}")       //where {from} and {to} are path variable  http://localhost:8000/currency-exchange/currency-exchange/from/USD/to/INR  
-public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to)  //from map to USD and to map to INR  
-{     
-return new  ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));  
-}  
-}  
+import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.core.env.Environment;
+@SpringBootApplication
+@RestController 
+public class CurrencyExchangeController 
+{
+@Autowired
+private Environment environment;
+@GetMapping("/currency-exchange/from/{from}/to/{to}")		//where {from} and {to} are path variable
+public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to)   //from map to USD and to map to INR
+{		
+ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+//setting the port
+exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+return exchangeValue;
+}
+}
